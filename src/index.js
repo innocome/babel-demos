a118adef31976a03037a4e27c8fb0b9602f49d08
baixiang360@163.com <baixiang360@163.com>
2017-03-09 18:28:52 +0800

    cm

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const sum = (a,b) => a + b;
+let x = sum(1,2);
+console.log(x);
---
+++ src/jsx.js
@@ -0,0 +1,7 @@
+import React,{Component} from 'react';
+
+class Demo extends Component{
+  render(){
+    return <h1>Hello World!!!</h1>
+  }
+}
